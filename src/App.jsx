4ce4d36eafe4bee679992331c3827e12976dc3cc
S P H K <ponharishkumars.21cse@kongu.edu>
2024-03-10 11:34:06 +0530

    profie fully completed createListing deleteListing updateListing also created

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-{/* <script src="http://localhost:8097"></script> */}
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home';
-import Nav from './components/Nav';
-import Signup from './pages/Signup';
-import SignIn from './pages/SignIn';
-import Profile from './pages/Profile';
-import React from 'react'
-import PrivateRoute from './components/PrivateRoute';
+{
+  /* <script src="http://localhost:8097"></script> */
+}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import Signup from "./pages/Signup";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import CreateListing from "./pages/CreateListing";
+import React from "react";
+import PrivateRoute from "./components/PrivateRoute";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Nav />
-    <Routes>
+      <Nav />
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Signup" element={<Signup/>} />
-        <Route path="/SignIn" element={<SignIn/>} />
-        <Route element={<PrivateRoute/>}>
-        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="create-listing" element={<CreateListing />}></Route>
+          <Route path="update-listing/:id" element={<UpdateListing />}></Route>
         </Route>
-        
-    </Routes>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -19,7 +19,8 @@ import {
   signoutFailure,
   signoutSuccess,
 } from "../redux/user/userSlice.js";
-import { set } from "mongoose";
+import { Link } from "react-router-dom";
+// import Listing from "../../../api/models/list.model.js";
 
 const Profile = () => {
   const fileRef = useRef(null);
@@ -29,7 +30,11 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setupdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
+
   const dispatch = useDispatch();
+  // console.log(userListings);
   // const user = currentUser.rest
 
   useEffect(() => {
@@ -72,18 +77,15 @@ const Profile = () => {
     try {
       dispatch(updateUserStart());
       // console.log(currentUser._id)
-      const res = await fetch(
-        `/api/user/update/${currentUser._id}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
       const data = await res.json();
-      console.log("data", data);
+      // console.log("data", data);
       if (data.success === false) {
         dispatch(updateUserFailure(data.message));
         return;
@@ -99,12 +101,9 @@ const Profile = () => {
   const handleDeleteUser = async () => {
     try {
       dispatch(deleteUserStart());
-      const res = await fetch(
-        `/api/user/delete/${currentUser._id}`,
-        {
-          method: "DELETE",
-        }
-      );
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
       const data = await res.json();
       if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
@@ -117,22 +116,60 @@ const Profile = () => {
     }
   };
 
-  const handlesignout= async()=>{
+  const handlesignout = async () => {
     try {
       dispatch(signoutStart());
-      const res=await fetch('/api/auth/signout');
-      const data=await res.json();
-      if(data.success===false){
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
         dispatch(signoutFailure(data.message));
         return;
       }
       dispatch(signoutSuccess(data));
-      
     } catch (error) {
       console.log(error.message);
       dispatch(signoutFailure(error.message));
     }
-  }
+  };
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+        });
+
+        const data = await res.json();
+        if (data.success === false) {
+          console.log(error.message);
+          return; 
+        }
+
+        setUserListings((prev) => prev.filter((listing) => (listing._id !== listingId)));
+
+
+
+    } catch (error) {
+      console.log(error.message);      
+    }
+
+  };
 
   return (
     <>
@@ -197,7 +234,14 @@ const Profile = () => {
           >
             {loading ? "loading..." : "Update"}
           </button>
+          <Link
+            to={"/create-listing"}
+            className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          >
+            create listing
+          </Link>
         </form>
+
         <p className="text-red-700 mt-5">{error ? error : ""}</p>
         <p className="text-green-700 mt-5">
           {updateSuccess ? "Updated Successfully!" : ""}
@@ -209,8 +253,55 @@ const Profile = () => {
           >
             Delete Account
           </span>
-          <span onClick={handlesignout} className="text-red-700 cursor-pointer">sign out</span>
+          <span onClick={handlesignout} className="text-red-700 cursor-pointer">
+            sign out
+          </span>
         </div>
+        <button onClick={handleShowListings} className="text-green-700 w-full">
+          Show List
+        </button>
+        <p className="text-red-700 mt-5">
+          {showListingsError ? "error showing listing" : ""}
+        </p>
+        {userListings && userListings.length > 0 && (
+          <div className="flex flex-col gap-4">
+            <h1 className="text-center mt-7 text-2xl font-semibold">
+              Your Listings
+            </h1>
+            {userListings.map((listing) => (
+              <div
+                key={listing._id}
+                className="border rounded-lg p-3 flex justify-between items-center gap-4"
+              >
+                <Link to={`/listing/${listing._id}`}>
+                  <img
+                    src={listing.imageURL[0]}
+                    alt="listing cover"
+                    className="h-16 w-16 object-contain"
+                  />
+                </Link>
+                <Link
+                  className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                  to={`/listing/${listing._id}`}
+                >
+                  <p>{listing.name}</p>
+                </Link>
+
+                <div className="flex flex-col item-center">
+                  <button
+                    onClick={() => handleListingDelete(listing._id)}
+                    className="text-red-700 uppercase"
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/update-listing/${listing._id}`}>
+                    <button className="text-green-700 uppercase">Edit</button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
